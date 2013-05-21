0a3300836a98ee0d429b81a71a13918df799c8b3
Chris Feltman <christopher.feltman@cgifederal.com>
2013-05-21 14:32:13 -0400

    Refactored to simplify code

--- ModalNightlight/ModalNightlight.ino
+++ ModalNightlight/ModalNightlight.ino
@@ -1,5 +1,12 @@
-#include <avr/interrupt.h>
+/*
+TODO: 
 
+  change ints to bytes
+  
+
+
+
+*/
 //operation modes
 const int TRANSITION_MODE  = 0; 
 const int RED_MODE         = 1; 
@@ -13,12 +20,10 @@ const int redPin = 11;
 const int greenPin = 10;
 const int bluePin = 9;
 const int buttonPin = 4;
-const int LED = 13;
 
 // operational parameter enums
-
-const int FULLY_OFF = 255;
-const int FULLY_ON = 0;
+const int FULLY_OFF = 255;   // RGB LED is common cathode
+const int FULLY_ON = 0;      // so we need reverse logic
 
 // led transition status
 const int OFF = 0;
@@ -35,43 +40,65 @@ const int RED = 0;
 const int GREEN = 1;
 const int BLUE = 2;
 
+// steady state outputs
+int outputValuesTable[6][3] = 
+{ 
+  {FULLY_OFF,FULLY_OFF,FULLY_OFF},     //TRANSITION MODE
+  {FULLY_ON, FULLY_OFF,FULLY_OFF},     //RED
+  {FULLY_OFF, FULLY_ON, FULLY_OFF},    //GREEN
+  {FULLY_OFF, FULLY_OFF, FULLY_ON},    // BLUE
+  {FULLY_ON, FULLY_ON, FULLY_OFF},     // YELLOW
+  {FULLY_OFF, FULLY_OFF, FULLY_OFF}    // OFF
+};
+
 // vars for transition mode
-int transitionStateTable[7][3];
+const int transitionStateTable[7][3] = 
+{
+  {RAMPING_UP, OFF, OFF},             // to RED
+  {HOLDING, OFF, RAMPING_UP},         // to PURPLE
+  {RAMPING_DOWN, OFF, HOLDING},       // TO BLUE
+  {OFF, RAMPING_UP, HOLDING},         // TO AQUA
+  {OFF, HOLDING, RAMPING_DOWN},       // TO GREEN
+  {RAMPING_UP, HOLDING, OFF},         // TO YELLOW
+  {RAMPING_DOWN, RAMPING_DOWN, OFF}   // TO OFF
+  
+};
 unsigned int transitionStateIndex = 0;
 unsigned int cycleCount = 0;
-int outputValues[3] = {FULLY_OFF, FULLY_OFF, FULLY_OFF};
 
 // output counter
 int counter = 0;
-const int OUTPUTCOUNT = 8192;
+const int OUTPUTCOUNT = 8192;   // adjust to change cycling rate of transition mode
 
 //switch debouncing
 // volatile to force the compiler to not optimize with these vars
 volatile int buttonState = HIGH; // initial state of the button
 volatile unsigned int shiftReg = 0xFF;    // keypress forces LO bit to 0
 
+/********************************************************************************************************
+Code
+*********************************************************************************************************/
 void setup()
 {
-  initTransitionStateTable(); 
-  Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-  
+  // set up LED pins
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT); 
-  pinMode(buttonPin, INPUT);
+  
+  // set up switch pin
+  pinMode(buttonPin, INPUT);     // pin 4 is switch input
   digitalWrite(buttonPin, HIGH); // turn on internal pull up resistor
   
-  analogWrite(redPin, FULLY_OFF);
-  analogWrite(greenPin, FULLY_OFF);
-  analogWrite(bluePin, FULLY_OFF);
+  // LEDS to OFF
+  writeToLeds();
+  
 }
 
 void loop()
 { 
   checkForModeChange(); 
-  
   counter++;
+  
   if(counter == OUTPUTCOUNT)   // output to pins each time the counter expires 
   {
     doOutput();
@@ -79,50 +106,21 @@ void loop()
   }
 }
 
-void initTransitionStateTable()
+void doOutput()   
 {
-  // to RED
-  transitionStateTable[0][RED] = RAMPING_UP;
-  transitionStateTable[0][GREEN] = OFF;
-  transitionStateTable[0][BLUE] = OFF;
-  
-  //to PURPLE
-  transitionStateTable[1][RED] = HOLDING;
-  transitionStateTable[1][GREEN] = OFF;
-  transitionStateTable[1][BLUE] = RAMPING_UP; 
-  
-  //to BLUE
-  transitionStateTable[2][RED] = RAMPING_DOWN;
-  transitionStateTable[2][GREEN] = OFF;
-  transitionStateTable[2][BLUE] = HOLDING;
-  
-  //to AQUA
-  transitionStateTable[3][RED] = OFF;
-  transitionStateTable[3][GREEN] = RAMPING_UP;
-  transitionStateTable[3][BLUE] = HOLDING;
+  if(currentMode == TRANSITION_MODE)
+  {  
+    doNextTransitionStep();
+  }
+  writeToLeds();
 
-  //to GREEN
-  transitionStateTable[4][RED] = OFF;
-  transitionStateTable[4][GREEN] = HOLDING;
-  transitionStateTable[4][BLUE] = RAMPING_DOWN;
-  
-  //to YELLOW
-  transitionStateTable[5][RED] = RAMPING_UP;
-  transitionStateTable[5][GREEN] = HOLDING;
-  transitionStateTable[5][BLUE] = OFF;
-  
-  // to DARK
-  transitionStateTable[6][RED] = RAMPING_DOWN;
-  transitionStateTable[6][GREEN] = RAMPING_DOWN;
-  transitionStateTable[6][BLUE] = OFF;   
-    
 }
 
 void checkForModeChange()
 {
   // debounce switch and check for button state change
   sampleSwitch();
-    
+
   if(shiftReg == 0 && buttonState == HIGH)
   {
     buttonState = LOW;
@@ -132,22 +130,7 @@ void checkForModeChange()
   {
     buttonState = HIGH;
   } 
-    
-}
 
-void sendDebugInfo()
-{
-  Serial.print("state ");
-  Serial.print(transitionStateIndex);
-  Serial.print(" cycle ");
-  Serial.print(cycleCount);
-  Serial.print(" red ");
-  Serial.print(outputValues[RED]);
-  Serial.print(" green ");
-  Serial.print(outputValues[GREEN]);
-  Serial.print(" blue ");
-  Serial.println(outputValues[BLUE]);  
-    
 }
 
 void sampleSwitch()
@@ -158,142 +141,71 @@ void sampleSwitch()
 
 }
 
-
 void initTransitionMode()
 {  
   transitionStateIndex = 0;
   cycleCount = 0;
-  outputValues[RED] = FULLY_OFF;
-  outputValues[GREEN] = FULLY_OFF;
-  outputValues[BLUE] = FULLY_OFF;
-  analogWrite(redPin, FULLY_OFF);
-  analogWrite(greenPin, FULLY_OFF);
-  analogWrite(bluePin, FULLY_OFF);
-  
+  outputValuesTable[TRANSITION_MODE][RED] = FULLY_OFF;
+  outputValuesTable[TRANSITION_MODE][GREEN] = FULLY_OFF;
+  outputValuesTable[TRANSITION_MODE][BLUE] = FULLY_OFF;
+ 
+  writeToLeds();
+
 }
+
 void doNextTransitionStep()
 {
 
-  for(int i = 0; i < 3; i++)
+  for(int i = 0; i < 3; i++)   // update the RGB values for the next output step
   {
-      int state = transitionStateTable[transitionStateIndex][i];
-           
-      switch(state)
-      {
-        case RAMPING_UP:
-          outputValues[i] = FULLY_OFF - cycleCount;
-          break;
-        case RAMPING_DOWN:
-          outputValues[i] = cycleCount;  
-          break;
-          /*
-        case HOLDING:
-          outputValues[i] = FULLY_ON;
-          break;
-        case OFF: 
-         outputValues[i] = FULLY_OFF; 
-         break; 
-        */ 
-      }     
+    int state = transitionStateTable[transitionStateIndex][i];   
+
+    switch(state)
+    {
+    case RAMPING_UP:
+      outputValuesTable[TRANSITION_MODE][i] = FULLY_OFF - cycleCount;
+      break;
+    case RAMPING_DOWN:
+      outputValuesTable[TRANSITION_MODE][i] = cycleCount;  
+      break;
+    default:       // if its anything else, leave it alone
+      break;
+    }     
   }
- 
-  // output calculated values;
-  analogWrite(redPin, outputValues[RED]);
-  analogWrite(greenPin, outputValues[GREEN]);
-  analogWrite(bluePin, outputValues[BLUE]);
- 
+
   cycleCount++;
-  
-  // advance state every 255 cycles
-  if(cycleCount == 256)
+ 
+  if(cycleCount == 256)   // advance state every 255 cycles
   {
     cycleCount = 0;
     transitionStateIndex++;
     transitionStateIndex = transitionStateIndex % 7;  // cycle 0-6
   }
-  
+
 }
 
-void doRed()
+void writeToLeds()
 {
-  analogWrite(redPin, FULLY_ON);
-  analogWrite(greenPin, FULLY_OFF);
-  analogWrite(bluePin, FULLY_OFF);
-}
-
-void doGreen()
-{
-  analogWrite(redPin, FULLY_OFF);
-  analogWrite(greenPin, FULLY_ON);
-  analogWrite(bluePin, FULLY_OFF);
-}
-
-void doBlue()
-{
-  analogWrite(redPin, FULLY_OFF);
-  analogWrite(greenPin, FULLY_OFF);
-  analogWrite(bluePin, FULLY_ON);
-}
-
-void doYellow()
-{
-  analogWrite(redPin, FULLY_ON);
-  analogWrite(greenPin, FULLY_ON);
-  analogWrite(bluePin, FULLY_OFF);
-}
-
-void doOff()
-{
-  analogWrite(redPin, FULLY_OFF);
-  analogWrite(greenPin, FULLY_OFF);
-  analogWrite(bluePin, FULLY_OFF);
+  analogWrite(redPin, outputValuesTable[currentMode][RED]);
+  analogWrite(greenPin, outputValuesTable[currentMode][GREEN]);
+  analogWrite(bluePin, outputValuesTable[currentMode][BLUE]);
 }
 
 void nextMode()
 {
 
-   if(currentMode == OFF_MODE)
-   {
-     currentMode = 0;
-   }
-   else
-   {
-     currentMode++;
-   }
-   
-   // need to reset vars or we will pick up where we left off  
-   if(currentMode == TRANSITION_MODE)
-   {
-     initTransitionMode();
-   } 
-}
-
-
-void doOutput()   // later we'll use function pointer table, KISS for now
-{
-
-   switch(currentMode)
+  if(currentMode == OFF_MODE)
   {
-    case TRANSITION_MODE:
-      doNextTransitionStep();
-      break;
-    case RED_MODE:
-      doRed();
-      break;
-    case GREEN_MODE: 
-      doGreen();
-      break;
-    case BLUE_MODE: 
-      doBlue();
-      break;
-    case YELLOW_MODE: 
-      doYellow();
-      break;
-    case OFF_MODE: 
-      doOff();
-      break;
+    currentMode = 0;
+  }
+  else
+  {
+    currentMode++;
   }
 
+  if(currentMode == TRANSITION_MODE)
+  {
+    initTransitionMode();
+  } 
 }
-
 
